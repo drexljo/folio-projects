47d4692584f5354c96ccafd9ab32ba2ac0eabb3d
ri86cox2 <johannes.drexl@lrz.de>
2020-11-17 15:36:47 +0100

    Final update before leaving

--- folio-mod-circulation/orig-packaging/modify.sh
+++ folio-mod-circulation/orig-packaging/modify.sh
@@ -11,4 +11,11 @@ OUTFILE="module_descriptor.json"
 ../../folio-buildhelpers/prep_md.py -i "$INFILE" -o "$OUTFILE" > /dev/null 2>&1 \
   && git add "$OUTFILE"
 
+if [ "$?" = "0" ]
+  then
+    # Copy the uniquely named relevant jar to standardized name
+    SRCNAME=$(grep "\"id\":" $BH_BINPATH/$PACKAGE/target/Activate.json | cut -d '"' -f 4)
+    cp "$BH_BINPATH/$PACKAGE/target/${SRCNAME}.jar" "$BH_BINPATH/$PACKAGE/target/mod-circulation-fat.jar"
+fi
+
 exit "$?"
